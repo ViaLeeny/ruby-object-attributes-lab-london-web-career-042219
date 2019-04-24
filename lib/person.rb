68f981c7095caeb04961ecd605716a442ba138c9
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-24 08:23:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person 
   
-  def their_name = (persons_name) 
-    @their_name = (persons_name)
+  def their_name=(persons_name) 
+    @their_name=(persons_name)
   end
   def their_name
     @their_name
